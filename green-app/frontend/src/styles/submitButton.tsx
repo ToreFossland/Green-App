a70550cd3433e9ea0b71830d1c15ebd34b07b9c9
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-06-29 12:31:49 +0200

    fix submitButton

--- green-app/frontend/src/styles/submitButton.tsx
+++ green-app/frontend/src/styles/submitButton.tsx
@@ -1,8 +1,9 @@
 import { styled } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import { createMuiTheme } from "@material-ui/core";
+import GlobalTheme from "../globalTheme";
 
-const theme = createMuiTheme();
+const theme = GlobalTheme;
 
 const MySubmitButton = styled(Button)({
   margin: theme.spacing(3, 0, 2),
