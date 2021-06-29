a4785b06b446f092a2d5dd1f0455936c9b0eb2fe
Therese <therese.sigmundstad@gmail.com>
2021-06-29 09:31:02 +0200

    Fix user authentication #4

--- green-app/backend/main.py
+++ green-app/backend/main.py
@@ -168,3 +168,12 @@ async def read_users_me(current_user: User = Depends(get_current_active_user)):
 @app.get("/users/me/items/")
 async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+@app.get("/users", tags=["users"])
+async def get_users() -> dict:
+    return { "data": users_db }
--- green-app/frontend/src/App.tsx
+++ green-app/frontend/src/App.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import Users from "./Users.json";
 import Login from "./Components/login";
 import Signup from "./Components/Signup";
 import Profile from "./Components/profile/profilePage";
 import UpdateProfile from "./Components/profile/updateProfile";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("https://cors-anywhere.herokuapp.com/http://localhost:8000/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(console.log);
+  });
+
   return (
     <BrowserRouter>
-    <div>
-      {/*
+      <div>
+        {/*
         A <Switch> looks through all its children <Route>
         elements and renders the first one whose path
         matches the current URL. Use a <Switch> any time
         you have multiple routes, but you want only one
         of them to render at a time
       */}
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/"  component={Signup} />
-        <Route exact path="/profile"  component={Profile} />
-        <Route exact path="/updateprofile"  component={UpdateProfile} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Signup} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/updateprofile" component={UpdateProfile} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
