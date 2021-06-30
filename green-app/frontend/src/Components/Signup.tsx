6def71d6a07c693ebb60d9d459fd52f1876d54e8
Benedicte <benedhr@ntnu.no>
2021-06-30 08:28:47 +0200

    styling for form

--- green-app/frontend/src/Components/Signup.tsx
+++ green-app/frontend/src/Components/Signup.tsx
@@ -1,12 +1,12 @@
 import Avatar from "@material-ui/core/Avatar";
-import SubmitButton from "../styles/StSubmitButton";
+import Form from "../styles/StForm";
+// import SubmitButton from "../styles/StSubmitButton";
 import { Button } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { useState } from "react";
 //import Users from "../Users.json";
@@ -14,16 +14,7 @@ import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import Paper from "../styles/StPaper";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    form: {
-      width: "100%",
-      marginTop: theme.spacing(3),
-    },
-  })
-);
 export default function SignUp() {
-  const classes = useStyles();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
@@ -38,7 +29,7 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <Form>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -92,7 +83,7 @@ export default function SignUp() {
             Sign Up
           </Button>
           <Grid container justify="flex-end"></Grid>
-        </form>
+        </Form>
       </Paper>
     </Container>
   );
--- green-app/frontend/src/styles/StPaper.tsx
+++ green-app/frontend/src/styles/StPaper.tsx
@@ -1,17 +1,17 @@
-import { styled } from '@material-ui/core/styles';
+import { styled } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import GlobalTheme from "../GlobalTheme";
 
 const theme = GlobalTheme;
 
-const MyPaper = styled(Paper) ({
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing(2),
-    textAlign: "left",
-    color: theme.palette.text.secondary,
+const MyPaper = styled(Paper)({
+  marginTop: theme.spacing(8),
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: theme.spacing(2),
+  textAlign: "left",
+  color: theme.palette.text.secondary,
 });
 
 export default MyPaper;
