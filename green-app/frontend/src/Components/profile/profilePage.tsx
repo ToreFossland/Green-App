5b78c71228bc6c351dddb48dd93e22e78d0165d4
Benedicte <benedhr@ntnu.no>
2021-07-01 08:26:46 +0200

    name changes

--- green-app/frontend/src/Components/profile/profilePage.tsx
+++ green-app/frontend/src/Components/profile/profilePage.tsx
@@ -1,33 +1,37 @@
-import ImageAvatars from '../../styles/StAvatar'
+import ImageAvatars from "../../styles/StAvatar";
 import users from "../../Users.json";
 //import { styled } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { Link as RouterLink } from 'react-router-dom';
-import StPaper from '../../styles/StPaper';
-import StHeader from '../../styles/StHeader';
-
+import Button from "@material-ui/core/Button";
+import { Link as RouterLink } from "react-router-dom";
+import StPaper from "../../styles/StPaper";
+import StHeader from "../../styles/StHeader";
 
 function ProfilePage() {
-
   return (
     <StPaper elevation={0}>
-        <StHeader>
-            <ImageAvatars/>
-            <h1> {users[3].name} </h1>
-        </StHeader>
-        <div>
-          {/* <p> {users[3].company} </p> */}
-          <p> {users[3].email} </p>
-          <h4> Dine poeng: {users[3].points} </h4>
-        </div>
+      <StHeader>
+        <ImageAvatars />
+        <h1> {users[3].name} </h1>
+      </StHeader>
+      <div>
+        {/* <p> {users[3].company} </p> */}
+        <p> {users[3].email} </p>
+        <h4> Dine poeng: {users[3].points} </h4>
+      </div>
 
-        <div>
-            <Button variant="contained" color="primary" component={RouterLink} to="/updateprofile" className="btn btn-primary" >
-              Oppdater profil
-            </Button>
-        </div>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          component={RouterLink}
+          to="/updateprofile"
+          className="btn btn-primary"
+        >
+          Oppdater profil
+        </Button>
+      </div>
     </StPaper>
   );
 }
 
-export default ProfilePage
+export default ProfilePage;
