d5c2f5be79de9874d9b1a182ef1c6944304559a0
thersi <therese.sigmundstad@gmail.com>
2021-07-08 14:53:27 +0200

    Edit switch style

--- frontend/src/styledComponents/StSwitch.tsx
+++ frontend/src/styledComponents/StSwitch.tsx
@@ -1,7 +1,76 @@
 import React from 'react';
-import Switch from '@material-ui/core/Switch';
+import { withStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch, { SwitchClassKey, SwitchProps } from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-export default function StSwitch() {
+interface Styles extends Partial<Record<SwitchClassKey, string>> {
+  focusVisible?: string;
+}
+
+interface Props extends SwitchProps {
+  classes: Styles;
+}
+
+const IOSSwitch = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(2),
+    },
+    switchBase: {
+      padding: 2,
+      '&$checked': {
+        transform: 'translateX(16px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          backgroundColor: 'primary',
+          opacity: 1,
+          border: 'none',
+        },
+      },
+      '&$focusVisible $thumb': {
+        //color: '#8FBC8F',
+        color: 'primary',
+        border: '6px solid #fff',
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      backgroundColor: theme.palette.grey[200],
+      opacity: 2,
+      transition: theme.transitions.create(['background-color', 'border']),
+    },
+    checked: {},
+    focusVisible: {},
+  })
+)(({ classes, ...props }: Props) => {
+  return (
+    <Switch
+      focusVisibleClassName={classes.focusVisible}
+      disableRipple
+      classes={{
+        root: classes.root,
+        switchBase: classes.switchBase,
+        thumb: classes.thumb,
+        track: classes.track,
+        checked: classes.checked,
+      }}
+      {...props}
+    />
+  );
+});
+
+export default function CustomizedSwitches() {
   const [state, setState] = React.useState({
     checked: false,
   });
@@ -11,14 +80,6 @@ export default function StSwitch() {
   };
 
   return (
-    <div>
-      <Switch
-        checked={state.checked}
-        onChange={handleChange}
-        name="checked"
-        color="primary"
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
-    </div>
+    <IOSSwitch checked={state.checked} onChange={handleChange} name="checked" />
   );
 }
