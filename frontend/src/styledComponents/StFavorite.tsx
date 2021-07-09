d95df17ad65dc521e7f4513beefa827d00dbb358
thersi <therese.sigmundstad@gmail.com>
2021-07-09 10:00:40 +0200

    Fix formatting

--- frontend/src/styledComponents/StFavorite.tsx
+++ frontend/src/styledComponents/StFavorite.tsx
@@ -4,19 +4,18 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
-
 export default function StFavorite() {
-    const [checked, setChecked] = React.useState(true);
-const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-
+  const [checked, setChecked] = React.useState(true);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
   };
-  return(
-  <Checkbox
-        checked={checked}
-        onChange={handleChange}
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-        icon={<FavoriteBorder />} checkedIcon={<Favorite />}
-      />
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'primary checkbox' }}
+      icon={<FavoriteBorder />}
+      checkedIcon={<Favorite />}
+    />
   );
 }
