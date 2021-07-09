8ef71e6fa3493d38d55d95be83d6e6a85a9ce346
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-07-09 18:41:11 +0200

    add default profile pic

--- frontend/src/styledComponents/StAvatar.tsx
+++ frontend/src/styledComponents/StAvatar.tsx
@@ -1,15 +1,19 @@
-import React from 'react'; 
+import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import { AccountCircle } from '@material-ui/icons';
 
 
 const StAvatar = styled(Avatar) ({
   display: 'flex',
+  fontSize: 60,
 });
 
 
 export default function styledAvatar() {
-const profilePic = false;
+  const profilePic = false;
 
-  return <StAvatar alt="profile picture" src={profilePic ? profilePic : "./logo192.png"}> </StAvatar>
+  return profilePic ? (<StAvatar alt="profile picture" src={profilePic}> </StAvatar>) : <AccountCircle style={{ fontSize: 60 }}/>
+
+
 }
