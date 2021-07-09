ffe8884b8abc033b1889a3f7e7e313f3f52b18cd
thersi <therese.sigmundstad@gmail.com>
2021-07-09 10:00:33 +0200

    Create activitylist item

---
+++ frontend/src/components/ActivityListItem.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import StSwitch from '../styledComponents/StSwitch';
+import StFavorite from '../styledComponents/StFavorite';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+
+export const ActivityListItem = () => {
+  return (
+    <div>
+      <ListItem>
+        <StFavorite />
+        <h3>Activity name</h3>
+        <StSwitch />
+      </ListItem>
+      <Divider component="li" />
+    </div>
+  );
+};
