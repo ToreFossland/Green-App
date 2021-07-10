117c81258dae47db72ce223942a7c557b73ef270
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-07-10 07:42:52 +0200

    Add padding to card/post

---
+++ frontend/src/styledComponents/StCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import { styled } from '@material-ui/core';
+import GlobalTheme from '../GlobalTheme';
+
+const theme = GlobalTheme;
+
+const StCard = styled(Card)({
+  paddingTop: theme.spacing(2),
+  paddingBottom: theme.spacing(2),
+  marginTop: theme.spacing(1),
+  marginBottom: theme.spacing(1),
+});
+
+export default StCard;
