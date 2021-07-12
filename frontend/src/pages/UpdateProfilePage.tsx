141630930807f2b99aea120363fd944e0830af51
Benedicte <benedihr@stud.ntnu.no>
2021-07-12 13:13:33 +0200

    follow stylingconvension for updateProfilePage

--- frontend/src/pages/UpdateProfilePage.tsx
+++ frontend/src/pages/UpdateProfilePage.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import StSubmitButton from 'styledComponents/StSubmitButton';
 import StAvatar from '../styledComponents/StAvatar';
 import StHeader from '../styledComponents/StHeader';
 import StPaper from '../styledComponents/StPaper';
-import { useContext, useState } from "react";
+import { useContext, useState } from 'react';
 import { GlobalContext } from 'state/context';
 import { useHistory } from 'react-router-dom';
 
 function UpdateProfilePage() {
-    const history = useHistory();
-    const [name, setName] = useState<string>('');
-    const { state, dispatch } = useContext(GlobalContext);
-    const [error, setError] = useState<string>('');
+  const history = useHistory();
+  const [name, setName] = useState<string>('');
+  const { state, dispatch } = useContext(GlobalContext);
+  const [error, setError] = useState<string>('');
 
-    return (
-        <StPaper elevation = {0}>
-            <StHeader>
-                <StAvatar/>
-                <div>
-                    <form noValidate autoComplete="off">
-                    <TextField id="changeName" label={name} variant="outlined" />
-                    </form>
-                </div>
-            </StHeader>
+  return (
+    <StPaper elevation={0}>
+      <StHeader>
+        <StAvatar />
+        <div>
+          <form noValidate autoComplete="off">
+            <TextField id="changeName" label={name} variant="outlined" />
+          </form>
+        </div>
+      </StHeader>
 
-              <Button variant="contained" color='primary' onClick={(e) => {
-                  e.preventDefault();
-                  console.log('click');
-                  }} >
-                  Save
-              </Button>
+      <StSubmitButton
+        onClick={(e) => {
+          e.preventDefault();
+          console.log('click');
+        }}
+      >
+        Save
+      </StSubmitButton>
     </StPaper>
   );
 }
