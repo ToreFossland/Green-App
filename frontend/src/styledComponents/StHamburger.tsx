5be520b3ca0b3edf84b5c80a5ff47fb56141e3b3
thersi <therese.sigmundstad@gmail.com>
2021-07-12 12:18:20 +0200

    Update logout routes in hamburger menu

--- frontend/src/styledComponents/StHamburger.tsx
+++ frontend/src/styledComponents/StHamburger.tsx
@@ -10,13 +10,13 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu'
-import SettingsIcon from '@material-ui/icons/Settings'
+import MenuIcon from '@material-ui/icons/Menu';
+import SettingsIcon from '@material-ui/icons/Settings';
 import LockIcon from '@material-ui/icons/Lock';
 import ReportIcon from '@material-ui/icons/Report';
 import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
 import { useHistory } from 'react-router-dom';
-import { isAuthenticated, logout } from '../utils/auth';
+import { logout } from '../utils/auth';
 
 const useStyles = makeStyles({
   list: {
@@ -38,28 +38,29 @@ export default function TemporaryDrawer() {
     right: false,
   });
 
-  const toggleDrawer = (anchor: Anchor, open: boolean) => (
-    event: React.KeyboardEvent | React.MouseEvent,
-  ) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
 
-    setState({ ...state, [anchor]: open });
-  };
+      setState({ ...state, [anchor]: open });
+    };
   const history = useHistory();
 
-  const handleClick=(item:string)=> {
-    logout();
-    history.push("/logout");
-  }
+  const handleClick = (item: number) => {
+    if (item === 1) {
+      logout();
+      history.push('/logout');
+    }
+  };
 
   const list = (anchor: Anchor) => (
-
     <div
       className={clsx(classes.list, {
         [classes.fullList]: anchor === 'top' || anchor === 'bottom',
@@ -69,9 +70,12 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['Settings',  'Scoreboard'].map((text, index) => (
+        {['Settings', 'Scoreboard'].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon> {index %2 === 0 ? <SettingsIcon />  : <EmojiEventsIcon/>} </ListItemIcon>
+            <ListItemIcon>
+              {' '}
+              {index % 2 === 0 ? <SettingsIcon /> : <EmojiEventsIcon />}{' '}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -79,8 +83,10 @@ export default function TemporaryDrawer() {
       <Divider />
       <List>
         {['Report problem', 'Logout'].map((text, index) => (
-          <ListItem button key={text} onClick={(e) => handleClick(text)}>
-            <ListItemIcon>{index %2 === 0 ?<ReportIcon />:<LockIcon />}</ListItemIcon>
+          <ListItem button key={index} onClick={(e) => handleClick(index)}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <ReportIcon /> : <LockIcon />}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -92,8 +98,14 @@ export default function TemporaryDrawer() {
     <div>
       {(['left'] as Anchor[]).map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}><MenuIcon/></Button>
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            <MenuIcon />
+          </Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
             {list(anchor)}
           </Drawer>
         </React.Fragment>
