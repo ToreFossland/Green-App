4fc5edb6da2b0d80bc467dda3ee4d1e750c3a068
Benedicte <benedihr@stud.ntnu.no>
2021-07-13 13:36:27 +0200

    change profile picture

--- frontend/src/pages/UpdateProfilePage.tsx
+++ frontend/src/pages/UpdateProfilePage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { Grid, Button, TextField, Container } from '@material-ui/core';
 import StSubmitButton from 'styledComponents/StSubmitButton';
 import StAvatar from '../styledComponents/StAvatar';
 import StHeader from '../styledComponents/StHeader';
@@ -10,29 +9,53 @@ import { GlobalContext } from 'state/context';
 import { useHistory } from 'react-router-dom';
 
 function UpdateProfilePage() {
-  const history = useHistory();
   const [name, setName] = useState<string>('');
   const { state, dispatch } = useContext(GlobalContext);
   const [error, setError] = useState<string>('');
+  const uploadedImage = React.useRef<image>(null);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const handleImageUpload: any = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      console.log(file);
+    }
+  };
 
   return (
     <StPaper elevation={0}>
+      <StAvatar />
+      <Container>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          multiple={false}
+        />
+        <img
+          ref={uploadedImage}
+          style={{
+            width: '100%',
+            height: '100%',
+            // position: 'absolute',
+          }}
+        />
+      </Container>
       <StHeader>
-        <StAvatar />
         <div>
+          <h3> New name: </h3>
           <form noValidate autoComplete="off">
             <TextField id="changeName" label={name} variant="outlined" />
           </form>
         </div>
       </StHeader>
-
       <StSubmitButton
         onClick={(e) => {
           e.preventDefault();
           console.log('click');
         }}
       >
-        Save
+        Save changes
       </StSubmitButton>
     </StPaper>
   );
