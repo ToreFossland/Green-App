81f93b40fd042c6b4cfcf00a06dd679f5ae24cf6
thersi <therese.sigmundstad@gmail.com>
2021-07-14 14:01:49 +0200

    Edit challenges.py

--- backend/app/api/routers/challenges.py
+++ backend/app/api/routers/challenges.py
@@ -1,14 +1,28 @@
-""" from fastapi import APIRouter, Request, Depends, Response, encoders
+from fastapi import APIRouter, Request, Depends, Response, encoders
 import typing as t
 
 from app.db.session import get_db
 from app.db.crud import (
+    get_challenges,
     seed_challenges
 )
-from app.db.schemas import Challenge
+from app.db.schemas import Challenge, ChallengeCreate
 from app.core.auth import get_current_active_user, get_current_active_superuser
 
-users_router = r = APIRouter()
+challenges_router = r = APIRouter()
+
+
+@r.post("/challenges", response_model=Challenge, response_model_exclude_none=True)
+async def user_create(
+    request: Request,
+    challenge: ChallengeCreate,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_superuser),
+):
+    """
+    Create a new challenge
+    """
+    return seed_challenges(db, challenge)
 
 
 @r.get(
@@ -21,8 +35,10 @@ async def challenges_list(
     db=Depends(get_db),
     current_user=Depends(get_current_active_superuser),
 ):
-    users = get_users(db)
+    """
+    Get all Challenges
+    """
+    challenges = get_challenges(db)
     # This is necessary for react-admin to work
-    response.headers["Content-Range"] = f"0-9/{len(users)}"
-    return users
- """
+    response.headers["Content-Range"] = f"0-9/{len(challenges)}"
+    return challenges
