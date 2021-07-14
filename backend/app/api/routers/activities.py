f2b18e80e51760d9886001e95d153dd17bd8578f
thersi <therese.sigmundstad@gmail.com>
2021-07-14 14:01:37 +0200

    Edit activities.py

--- backend/app/api/routers/activities.py
+++ backend/app/api/routers/activities.py
@@ -6,10 +6,23 @@ from app.db.crud import (
     get_activities,
     seed_activities
 )
-from app.db.schemas import Activity
+from app.db.schemas import Activity, ActivityCreate
 from app.core.auth import get_current_active_user, get_current_active_superuser
 
-users_router = r = APIRouter()
+activities_router = r = APIRouter()
+
+
+@r.post("/activities", response_model=Activity, response_model_exclude_none=True)
+async def user_create(
+    request: Request,
+    activity: ActivityCreate,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_superuser),
+):
+    """
+    Create a new activity
+    """
+    return seed_activities(db, activity)
 
 
 @r.get(
