dc182240be617288ca1b6517d63bbcb36ad7d7e4
thersi <therese.sigmundstad@gmail.com>
2021-07-14 11:21:10 +0200

    Create activity context

---
+++ frontend/src/utils/activity.ts
@@ -0,0 +1,34 @@
+import IActivity from "interfaces/IActivity";
+import { useState } from "react";
+
+const getActivities = async() : Promise<IActivity> =>{
+    let token:string = localStorage.getItem('token')||'{}';
+    let httpHeaders = { 
+      'Content-Type' : 'application/x-www-form-urlencoded', 
+      'Accept' : 'application/json',
+      'Authorization' : `Bearer ${token}`
+    };
+    const request = new Request('/api/v1/activities', {
+      method: 'GET',
+      headers: new Headers(httpHeaders)
+      },
+    );
+
+  
+    const response = await fetch(request);
+  
+    if (response.status === 500) {
+      throw new Error('Internal server error');
+    }
+  
+    const data = await response.json();
+    if (response.status > 400 && response.status < 500) {
+      if (data.detail) {
+        throw data.detail;
+      }
+      throw data;
+    }
+    return data;
+  }
+
+export default getActivities;
