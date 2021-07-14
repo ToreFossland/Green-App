8e1334b105dc754db54a02348f32a437ab476dd7
thersi <therese.sigmundstad@gmail.com>
2021-07-14 11:20:10 +0200

    Create activities.py

---
+++ backend/app/api/routers/activities.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Request, Depends, Response, encoders
+import typing as t
+
+from app.db.session import get_db
+from app.db.crud import (
+    get_activities,
+    seed_activities
+)
+from app.db.schemas import Activity
+from app.core.auth import get_current_active_user, get_current_active_superuser
+
+users_router = r = APIRouter()
+
+
+@r.get(
+    "/activities",
+    response_model=t.List[Activity],
+    response_model_exclude_none=True,
+)
+async def activities_list(
+    response: Response,
+    db=Depends(get_db),
+    current_user=Depends(get_current_active_superuser),
+):
+    """
+    Get all Activities
+    """
+    activities = get_activities(db)
+    # This is necessary for react-admin to work
+    response.headers["Content-Range"] = f"0-9/{len(activities)}"
+    return activities
