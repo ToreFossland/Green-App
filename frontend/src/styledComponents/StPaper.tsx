6fa471d12859c6e334da9e380580c8146d602d9d
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-07-16 08:34:17 +0200

    fix color

--- frontend/src/styledComponents/StPaper.tsx
+++ frontend/src/styledComponents/StPaper.tsx
@@ -13,7 +13,7 @@ const StPaper = styled(Paper)({
   marginTop: theme.spacing(1),
   marginBottom: theme.spacing(1),
   textAlign: 'left',
-  color: theme.palette.primary.main,
+  color: theme.palette.text.primary,
   width: '100%',
 });
 
