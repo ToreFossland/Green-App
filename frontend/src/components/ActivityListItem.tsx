793747cddcc0400b83a1d57deba0f123a0e1e0fa
thersi <therese.sigmundstad@gmail.com>
2021-07-26 08:28:55 +0200

    Add slider to addActivity

--- frontend/src/components/ActivityListItem.tsx
+++ frontend/src/components/ActivityListItem.tsx
@@ -3,14 +3,18 @@ import StSwitch from '../styledComponents/StSwitch';
 import StFavorite from '../styledComponents/StFavorite';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
+import SliderEffort from './SliderEffort';
 
 export const ActivityListItem = () => {
   return (
     <div>
       <ListItem style={{ justifyContent: 'center', width: '100%' }}>
-        <StFavorite />
-        <label style={{ fontSize: '14pt' }}>Activity name</label>
-        <StSwitch />
+        <div>
+          <StFavorite />
+          <label style={{ fontSize: '14pt' }}>Activity name</label>
+          <StSwitch />
+          <SliderEffort />
+        </div>
       </ListItem>
       <Divider component="li" />
     </div>
