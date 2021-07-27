57cc1a657fdacb4d28ba0b5ae75fdb41f9000d08
thersi <therese.sigmundstad@gmail.com>
2021-07-27 10:18:42 +0200

    Fix routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,13 +1,14 @@
 import React, { FC, useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from "@material-ui/styles";
+import { ThemeProvider } from '@material-ui/styles';
 import { PrivateRoute } from './components/PrivateRoute';
 import { Admin } from 'admin/Admin';
 import Profile from './pages/ProfilePage';
 import HomePage from 'pages/HomePage';
 import LoginPage from 'pages/LoginPage';
+import MapPage from 'pages/MapPage';
 import { SignUpPage } from 'pages/SignUpPage';
-import GlobalTheme from "./GlobalTheme";
+import GlobalTheme from './GlobalTheme';
 import MyAppBar from './components/MyAppBar';
 import MyBottomNavigation from './components/MyBottomNavigation';
 import UpdateProfilePage from './pages/UpdateProfilePage';
@@ -17,37 +18,42 @@ import SettingsPage from 'pages/SettingsPage';
 import StBackgroundPaper from 'styledComponents/StBackgroundPaper';
 import ScoreboardPage from 'pages/ScoreboardPage';
 
-
 export const Routes: FC = () => {
   const { state } = useContext(GlobalContext);
-  
 
   return (
     <ThemeProvider theme={GlobalTheme}>
-      <MyAppBar/>
+      <MyAppBar />
       <StBackgroundPaper>
         <Switch>
-            <Route path="/admin">
-              <Admin />
-            </Route>
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/signup" component={SignUpPage} />
-            <Route
-              exact path="/logout" component={LoginPage}
-              // render={() => {
-              //   logout();
-              //   return null;
-              // }}
-            />
-            <PrivateRoute exact path="/" component={HomePage} />
-            <PrivateRoute exact path="/profile" component={Profile} />
-            <PrivateRoute exact path="/updateprofile" component={UpdateProfilePage} />
-            <PrivateRoute exact path="/add" component={AddActivities} />
-            <PrivateRoute exact path="/settings" component={SettingsPage} />
-            <PrivateRoute exact path="/scoreboard" component={ScoreboardPage} />
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignUpPage} />
+          <Route
+            exact
+            path="/logout"
+            component={LoginPage}
+            // render={() => {
+            //   logout();
+            //   return null;
+            // }}
+          />
+          <PrivateRoute exact path="/" component={HomePage} />
+          <PrivateRoute exact path="/profile" component={Profile} />
+          <PrivateRoute
+            exact
+            path="/updateprofile"
+            component={UpdateProfilePage}
+          />
+          <PrivateRoute exact path="/add" component={AddActivities} />
+          <PrivateRoute exact path="/settings" component={SettingsPage} />
+          <PrivateRoute exact path="/scoreboard" component={ScoreboardPage} />
+          <PrivateRoute exact path="/map" component={MapPage} />
         </Switch>
       </StBackgroundPaper>
-      <MyBottomNavigation/>
+      <MyBottomNavigation />
     </ThemeProvider>
   );
 };
