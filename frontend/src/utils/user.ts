52880d90bef8aaf0654f1c61c460336568bfe6d6
Benedicte <benedihr@stud.ntnu.no>
2021-07-28 14:29:47 +0200

    function in user.ts to get all users

--- frontend/src/utils/user.ts
+++ frontend/src/utils/user.ts
@@ -1,33 +1,59 @@
-import IUser from "interfaces/IUser";
+import IUser from 'interfaces/IUser';
 
-const getUser = async() : Promise<IUser> =>{
-    let token:string = localStorage.getItem('token')||'{}';
-    let httpHeaders = {
-      'Content-Type' : 'application/x-www-form-urlencoded',
-      'Accept' : 'application/json',
-      'Authorization' : `Bearer ${token}`
-    };
-    const request = new Request('/api/users/me', {
-      method: 'GET',
-      headers: new Headers(httpHeaders)
-      },
-    );
+const getUser = async (): Promise<IUser> => {
+  let token: string = localStorage.getItem('token') || '{}';
+  let httpHeaders = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+  const request = new Request('/api/users/me', {
+    method: 'GET',
+    headers: new Headers(httpHeaders),
+  });
 
+  const response = await fetch(request);
 
-    const response = await fetch(request);
-
-    if (response.status === 500) {
-      throw new Error('Internal server error');
-    }
-
-    const data = await response.json();
-    if (response.status > 400 && response.status < 500) {
-      if (data.detail) {
-        throw data.detail;
-      }
-      throw data;
-    }
-    return data;
+  if (response.status === 500) {
+    throw new Error('Internal server error');
   }
+
+  const data = await response.json();
+  if (response.status > 400 && response.status < 500) {
+    if (data.detail) {
+      throw data.detail;
+    }
+    throw data;
+  }
+  return data;
+};
+
+const getUsers = async (): Promise<IUser> => {
+  let token: string = localStorage.getItem('token') || '{}';
+  let httpHeaders = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+  const request = new Request('/api/users/', {
+    method: 'GET',
+    headers: new Headers(httpHeaders),
+  });
+
+  const response = await fetch(request);
+
+  if (response.status === 500) {
+    throw new Error('Internal server error');
+  }
+
+  const data = await response.json();
+  if (response.status > 400 && response.status < 500) {
+    if (data.detail) {
+      throw data.detail;
+    }
+    throw data;
+  }
+  return data;
+};
 
 export default getUser;
