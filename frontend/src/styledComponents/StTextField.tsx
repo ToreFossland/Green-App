95be470fefa1e4b7ea6e30fec4d60dc37d598664
Benedicte <benedihr@stud.ntnu.no>
2021-07-28 12:17:45 +0200

    remove profilePicture logic

--- frontend/src/styledComponents/StTextField.tsx
+++ frontend/src/styledComponents/StTextField.tsx
@@ -1,7 +1,7 @@
 import { TextField } from '@material-ui/core';
 import { styled, withTheme } from '@material-ui/core';
 
-const StTextField = styled(withTheme(TextField))(props => ({
+const StTextField = styled(withTheme(TextField))((props) => ({
   marginTop: props.theme.spacing(2),
   width: '80%',
 }));
