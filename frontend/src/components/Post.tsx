ede9ebc87044243bd6076f2d8755dace76eb97be
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-07-28 15:58:55 +0200

    add effort and points to posts

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -52,7 +52,7 @@ const Post = (props : any) => {
                     {props.activityName}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                    ... effort. ... points.
+                    {props.effort} effort. {props.points} points.
                 </Typography>
             </CardContent>
             <CardMedia
