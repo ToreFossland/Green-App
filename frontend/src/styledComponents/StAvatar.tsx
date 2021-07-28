af7065b62ea6296bca0041f079a6c78006ded49a
Benedicte <benedihr@stud.ntnu.no>
2021-07-28 11:07:06 +0200

    adding nameIntalis to styledAvatar

--- frontend/src/styledComponents/StAvatar.tsx
+++ frontend/src/styledComponents/StAvatar.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import { AccountCircle } from '@material-ui/icons';
+import { useContext } from 'react';
+import { GlobalContext } from 'state/context';
+import React from 'react';
 
-
-const StAvatar = styled(Avatar) ({
+const StAvatar = styled(Avatar)({
   display: 'flex',
   fontSize: 60,
 });
 
+const StyledAvatar = () => {
+  const { state } = useContext(GlobalContext);
+  const firstLetter: any = state.user?.first_name.charAt(0);
+  const secondLetter: any = state.user?.last_name.charAt(0);
+  const nameInitials: string = firstLetter.concat(secondLetter);
+  return <StAvatar>{nameInitials}</StAvatar>;
+};
 
-export default function styledAvatar() {
-  const profilePic = false;
-
-  return profilePic ? (<StAvatar alt="profile picture" src={profilePic}> </StAvatar>) : <AccountCircle style={{ fontSize: 60 }}/>
-
-
-}
+export default StyledAvatar;
