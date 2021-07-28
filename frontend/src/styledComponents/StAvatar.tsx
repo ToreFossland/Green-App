dda8a89659a031c29d622184589d232fd128a5e0
Benedicte <benedihr@stud.ntnu.no>
2021-07-28 11:20:58 +0200

    fix fontSize

--- frontend/src/styledComponents/StAvatar.tsx
+++ frontend/src/styledComponents/StAvatar.tsx
@@ -7,15 +7,18 @@ import React from 'react';
 
 const StAvatar = styled(Avatar)({
   display: 'flex',
-  fontSize: 60,
+  fontSize: 20,
 });
 
 const StyledAvatar = () => {
   const { state } = useContext(GlobalContext);
-  const firstLetter: any = state.user?.first_name.charAt(0);
-  const secondLetter: any = state.user?.last_name.charAt(0);
-  const nameInitials: string = firstLetter.concat(secondLetter);
-  return <StAvatar>{nameInitials}</StAvatar>;
+  if (state.user?.first_name && state.user?.last_name) {
+    const firstLetter: any = state.user?.first_name.charAt(0);
+    const secondLetter: any = state.user?.last_name.charAt(0);
+    const nameInitials: string = firstLetter.concat(secondLetter);
+    return <StAvatar>{nameInitials}</StAvatar>;
+  }
+  return <StAvatar></StAvatar>;
 };
 
 export default StyledAvatar;
