4067772df5f2439a3daa060d40c01814e6447c30
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-07-29 10:47:56 +0200

    add snackbar

--- frontend/src/components/SocialGrid.tsx
+++ frontend/src/components/SocialGrid.tsx
@@ -2,21 +2,36 @@ import React, { useContext, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Post from './Post';
 import { GlobalContext } from 'state/context';
+import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
 
 function SocialGrid() {
     const {state } = useContext(GlobalContext);
     const [deleted, setDeleted] = useState<boolean>(false);
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        setDeleted(false);
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
 
     return (
         <Grid container direction="column" justifyContent="center" alignItems="stretch" >
             {/* item[2].id for å finne ut hvilken idrett */}
             {deleted && (
                 <Grid item>
-                    <Alert severity="info" onClose={() => {setDeleted(false)}} > Post was deleted </Alert>
+                    <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open} autoHideDuration={6000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="info">
+                            Post was deleted
+                        </Alert>
+                    </Snackbar>
                 </Grid>
                 )}
-                
+
             {state.performsActivities && state.performsActivities?.slice(0).reverse().map((item: any) =>
                 // <Grid item>
                     <Post key= {item[1].id}
@@ -24,7 +39,7 @@ function SocialGrid() {
                     firstName = {item[0].first_name} lastName = {item[0].last_name}
                     activityName = {item[2].name} date = {item[1].date}
                     points = {item[2].points} effort = {item[1].effort}
-                    setDeleted={setDeleted} />
+                    setDeleted={setDeleted} setOpen={setOpen} />
                 // </Grid>
                 )}
         </Grid>
