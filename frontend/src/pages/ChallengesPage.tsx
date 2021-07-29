e325ef6d4133795f2ef214d2d17592885b316570
thersi <therese.sigmundstad@gmail.com>
2021-07-29 15:57:12 +0200

    Update ChallengesPage

--- frontend/src/pages/ChallengesPage.tsx
+++ frontend/src/pages/ChallengesPage.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import SocialGrid from 'components/SocialGrid';
-import HomePageLinks from 'components/HomePageLinks';
-import { ChallengeListItem } from 'components/ChallengeListItem';
 import { GlobalContext } from 'state/context';
 import List from '@material-ui/core/List';
 import getChallenges from 'utils/challenge';
+import ScrollingDialog from 'components/ScrollingDialog';
 const ChallengesPage = () => {
-  console.log('here 1');
   const { state } = React.useContext(GlobalContext);
   const challenges = state?.challenges;
-  console.log(state);
   return (
     <div>
       <List style={{ width: '100%' }}>
         {challenges?.map((item) => (
-          <ChallengeListItem
+          <ScrollingDialog
             key={item.id}
             id={item.id}
             name={item.name}
