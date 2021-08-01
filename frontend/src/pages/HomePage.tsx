67ca13932ef95ee4de4c64261c651c7a78eda5d1
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-08-01 23:33:09 +0200

    remove home page links

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import HomePageLinks from 'components/HomePageLinks';
 import ActivityTabs from 'components/ActivityTabs';
 
 const HomePage = () => {
   return (
     <div>
-      <HomePageLinks />
       <ActivityTabs/>
     </div>
   );
