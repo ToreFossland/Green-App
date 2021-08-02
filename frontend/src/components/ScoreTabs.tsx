1cd2ceb775c035151ad3e8d1988219299c226219
Benedicte <benedihr@stud.ntnu.no>
2021-08-02 10:06:57 +0200

    ScoreTabs for tabbing between score-related

---
+++ frontend/src/components/ScoreTabs.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Table from './Table';
+import List from '@material-ui/core/List';
+import ScrollingDialog from 'components/ScrollingDialog';
+import { GlobalContext } from 'state/context';
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography component={'span'} variant={'body2'}>
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: '100%',
+  },
+}));
+
+export default function ActivityTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+  const { state } = React.useContext(GlobalContext);
+  const challenges = state?.challenges;
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Challenges" {...a11yProps(0)} />
+          <Tab label="Scoreboard" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          <List style={{ width: '100%' }}>
+            {challenges?.map((item) => (
+              <ScrollingDialog
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                description={item.description}
+                points={item.points}
+                activity_id={item.activity_id}
+              />
+            ))}
+          </List>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          <Table />
+        </TabPanel>
+      </SwipeableViews>
+    </div>
+  );
+}
