5fd4e749607b1ecd1f52af52460e372ae74caea4
Benedicte <benedihr@stud.ntnu.no>
2021-08-02 11:23:37 +0200

    added tabs

--- frontend/src/pages/ChallengesPage.tsx
+++ frontend/src/pages/ChallengesPage.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { GlobalContext } from 'state/context';
-import List from '@material-ui/core/List';
-import ScrollingDialog from 'components/ScrollingDialog';
+import ScoreTabs from 'components/ScoreTabs';
 
 const ChallengesPage = () => {
   const { state } = React.useContext(GlobalContext);
   const challenges = state?.challenges;
   return (
     <div>
-      <List style={{ width: '100%' }}>
-        {challenges?.map((item) => (
-          <ScrollingDialog
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            points={item.points}
-            activity_id={item.activity_id}
-          />
-        ))}
-      </List>
+      <ScoreTabs />
     </div>
   );
 };
