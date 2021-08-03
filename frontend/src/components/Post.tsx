81507666975097515168fbed87ae3a9d33d9d0a0
thersi <therese.sigmundstad@gmail.com>
2021-08-03 15:55:02 +0200

    Add only current user can edit or delete

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -8,7 +8,9 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import CommentIcon from '@material-ui/icons/Comment';
 import ShareIcon from '@material-ui/icons/Share';
 import StCard from 'styledComponents/StCard';
-import getPerformsActivities, { deletePerformsActivity } from 'utils/performsActivities';
+import getPerformsActivities, {
+  deletePerformsActivity,
+} from 'utils/performsActivities';
 import { GlobalContext } from 'state/context';
 import { performsActivities } from 'state/performsActivities/performsActivitiesActions';
 import StAvatarFeed from 'styledComponents/StAvatarFeed';
@@ -19,97 +21,130 @@ import { user } from 'state/user/userActions';
 import Grid from '@material-ui/core/Grid';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBiking, faCarrot, faTrashAlt, faTshirt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBiking,
+  faCarrot,
+  faTrashAlt,
+  faTshirt,
+} from '@fortawesome/free-solid-svg-icons';
 
+const Post = (props: any) => {
+  const { dispatch, state } = useContext(GlobalContext);
+  const currentUser = state.user!;
+  let name = `${props.firstName} ${props.lastName}`;
+  const [liked, setLiked] = useState<boolean>(false);
+  const [likeButtonColor, setLikeButtonColor] = useState<
+    'default' | 'secondary' | 'inherit' | 'primary' | undefined
+  >('default');
 
-const Post = (props : any) => {
-    const { dispatch, state } = useContext(GlobalContext);
-    const currentUser = state.user!;
-    let name = `${props.firstName} ${props.lastName}`
-    const [liked, setLiked] = useState<boolean>(false);
-    const [likeButtonColor, setLikeButtonColor] = useState<"default" | "secondary" | "inherit" | "primary" | undefined >("default");
-
-    const likeButtonColorChange = async () => {
-        if (!liked) {
-            setLikeButtonColor('secondary');
-        } else {
-            setLikeButtonColor('default');
-        }
+  const likeButtonColorChange = async () => {
+    if (!liked) {
+      setLikeButtonColor('secondary');
+    } else {
+      setLikeButtonColor('default');
     }
+  };
 
-    const onLikeButtonClick = () => {
-        setLiked(!(liked));
-        likeButtonColorChange();
-    };
+  const onLikeButtonClick = () => {
+    setLiked(!liked);
+    likeButtonColorChange();
+  };
 
-    const onDelete = async () => {
-        deletePerformsActivity(props.id);
-        const myPerformsActivities = await getPerformsActivities();
-        dispatch(performsActivities(myPerformsActivities));
-        props.setDeleted(true);
-        props.setOpen(true);
-        try {
-            const data = await updateUser(currentUser.id, currentUser.email, (currentUser.points - props.points));
+  const onDelete = async () => {
+    deletePerformsActivity(props.id);
+    const myPerformsActivities = await getPerformsActivities();
+    dispatch(performsActivities(myPerformsActivities));
+    props.setDeleted(true);
+    props.setOpen(true);
+    try {
+      const data = await updateUser(
+        currentUser.id,
+        currentUser.email,
+        currentUser.points - props.points
+      );
 
-            if (data) {
-              const myUser = await getUser();
-              dispatch(user(myUser));
-            }
-          } catch (err) {
-            if (err instanceof Error) {
-              // handle errors thrown from frontend
-              props.setError(err.message);
-              console.log(props.error);
-            } else {
-              // handle errors thrown from backend
-              props.setError(err);
-              console.log(props.error);
-            }
-          }
-    };
+      if (data) {
+        const myUser = await getUser();
+        dispatch(user(myUser));
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        // handle errors thrown from frontend
+        props.setError(err.message);
+        console.log(props.error);
+      } else {
+        // handle errors thrown from backend
+        props.setError(err);
+        console.log(props.error);
+      }
+    }
+  };
 
-    let timestamp : number = +props.date;
-    let date : Date = new Date(timestamp);
+  let timestamp: number = +props.date;
+  let date: Date = new Date(timestamp);
 
-    return (
-        <StCard>
-            <CardHeader
-                avatar={<StAvatarFeed firstname={props.firstName} lastname={props.lastName} />}
-                title={name}
-                subheader={date.toDateString()}
-                action={ <PostMenu onDelete={onDelete} /> }
-            />
-            <Grid container direction="row" justifyContent="flex-start" alignItems="stretch" >
-                <CardContent>
-                    <Typography variant="h6" color="textSecondary" component="p">
-                        {props.activityName}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.effort} effort. {props.points} points.
-                    </Typography>
-                </CardContent>
-                <CardContent>
-                    <Typography variant="h6" color="primary" component="p" >
-                        {props.activityName === "Bike to work" && (<FontAwesomeIcon icon={faBiking} />)}
-                        {props.activityName === "Eat a vegetarian meal" && (<FontAwesomeIcon icon={faCarrot} />)}
-                        {props.activityName === "Pick up trash" && (<FontAwesomeIcon icon={faTrashAlt} />)}
-                        {props.activityName === "Donate old clothes" && (<FontAwesomeIcon icon={faTshirt} />)}
-                    </Typography>
-                </CardContent>
-            </Grid>
-            <CardActions style={{alignItems: 'center', justifyContent: 'space-between'}} >
-                <IconButton aria-label="add to favorites" color={likeButtonColor} onClick={onLikeButtonClick} >
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="add a comment">
-                    <CommentIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
-            </CardActions>
-        </StCard>
-    )
-}
+  return (
+    <StCard>
+      <CardHeader
+        avatar={
+          <StAvatarFeed firstname={props.firstName} lastname={props.lastName} />
+        }
+        title={name}
+        subheader={date.toDateString()}
+        action={
+          currentUser.id === props.user_id && <PostMenu onDelete={onDelete} />
+        }
+      />
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="stretch"
+      >
+        <CardContent>
+          <Typography variant="h6" color="textSecondary" component="p">
+            {props.activityName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.effort} effort. {props.points} points.
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <Typography variant="h6" color="primary" component="p">
+            {props.activityName === 'Bike to work' && (
+              <FontAwesomeIcon icon={faBiking} />
+            )}
+            {props.activityName === 'Eat a vegetarian meal' && (
+              <FontAwesomeIcon icon={faCarrot} />
+            )}
+            {props.activityName === 'Pick up trash' && (
+              <FontAwesomeIcon icon={faTrashAlt} />
+            )}
+            {props.activityName === 'Donate old clothes' && (
+              <FontAwesomeIcon icon={faTshirt} />
+            )}
+          </Typography>
+        </CardContent>
+      </Grid>
+      <CardActions
+        style={{ alignItems: 'center', justifyContent: 'space-between' }}
+      >
+        <IconButton
+          aria-label="add to favorites"
+          color={likeButtonColor}
+          onClick={onLikeButtonClick}
+        >
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="add a comment">
+          <CommentIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </StCard>
+  );
+};
 
 export default Post;
